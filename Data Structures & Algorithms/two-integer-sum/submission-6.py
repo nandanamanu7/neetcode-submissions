dfeef49857bd2e385fff42a23ed4ef70ec1ef883
Nandana Manu <165932853+nandanamanu7@users.noreply.github.com>
2026-09-04 14:06:01 -0700

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i in range(len(nums)):
+            if (target - nums[i]) in seen:
+                return [min(seen.get(target-nums[i]), i), max(seen.get(target-nums[i]), i)]
+            seen[nums[i]] = seen.get(nums[i], i)
+
+        
